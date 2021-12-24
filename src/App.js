22f4b85aa066c58c7448b3a21f4fadb43b74c9e5
Silviu <nistorescu.silviu@gmail.com>
2021-12-24 10:56:38 +0200

    added multiform

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import "./App.css";
+import MultiForm from"./components/MultiForm.js";
 
+function App(){
+  return(
+    <div className="App">
+      <MultiForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MultiForm.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const MultiForm = () => {
+  return <div></div>;
+};
+
+export default MultiForm;
